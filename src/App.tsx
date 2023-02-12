0440c6384678926336d4c3102f0423a4c9a094a5
maxnankivell <maxnankivell1@gmail.com>
2023-02-12 14:02:42 +1300

    added dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.scss';
 import Tower from './components/Tower';
 
 function App() {
   return (
-    <div className='app'>
-      <h1 className='title'>Tower of Hanoi</h1>
-      <Tower className='tower1' />
-      <Tower className='tower2' />
-      <Tower className='tower3' />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className='app'>
+        <h1 className='title'>Tower of Hanoi</h1>
+        <Tower className='tower1' />
+        <Tower className='tower2' />
+        <Tower className='tower3' />
+      </div>
+    </DndProvider>
   );
 }
 
